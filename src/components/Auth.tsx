8c17241b8803a19b69d0a7cd4fc63128aa794c04
unknown <sanyapridava@mail.ru>
2024-07-14 22:44:37 +0300

    Page with form for auth

---
+++ src/components/Auth.tsx
@@ -0,0 +1,27 @@
+import '../styles/Auth.css';
+import {useNavigate} from "react-router-dom";
+
+export default function Auth() {
+    const navigate = useNavigate();
+    const return_to_reg = ()=>{
+        navigate("/reg");
+    }
+
+    return (
+        <div id="PageAuthForm">
+            <h1>Вход</h1>
+            <form>
+                <div className={"field"}>
+                    <label>Почта</label>
+                    <input/>
+                </div>
+                <div className={"field"}>
+                    <label>Пароль</label>
+                    <input type={"password"}/>
+                </div>
+                <button>Войти</button>
+                <button id={"reg"} onClick={return_to_reg}>Зарегестрироваться?</button>
+            </form>
+        </div>
+    )
+}
