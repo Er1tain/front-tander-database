4888241f9cf54306013484319f0ee6ae0930ba4e
unknown <sanyapridava@mail.ru>
2024-07-14 22:46:27 +0300

    Import components

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Welcome from "./components/Welcome";
+import Auth from "./components/Auth";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
