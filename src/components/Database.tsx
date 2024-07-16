4e49529f5cec00f25ed9996531aba943177687f1
Alexander Pridava <sanyapridava@mail.ru>
2024-07-16 20:08:53 +0300

    Add button panel

--- src/components/Database.tsx
+++ src/components/Database.tsx
@@ -1,8 +1,15 @@
+import Table from "./Database/Table";
+import '../styles/Database.css';
 
 export default function Database() {
     return (
         <div id={"database"}>
-
+            <h1>Учёт устройств</h1>
+            <Table/>
+            <div id="button_panel">
+                <button id="back"></button>
+                <button id="next"></button>
+            </div>
         </div>
     )
 }
